021fbc7afd973fd378cf3b7cb39bf0f057c21b5b
sijan-5 <bhattaraisijan22@gmail.com>
2025-04-23 23:52:27 +0545

    Debugging Of Feature Wt Ranking

--- feature_weight_update/feature_weight_update.py
+++ feature_weight_update/feature_weight_update.py
@@ -1,6 +1,5 @@
 import math
 import numpy as np
-import random
 
 # Generate a longer random feature list
 global_feature_list = {
@@ -18,39 +17,39 @@ del_v = 0
 # Initialize important and unimportant feature lists
 important_features = {}
 unimportant_features = {}
-
-def compute():
-    global important_features, unimportant_features, del_u, del_v
-    first_time = True
-
-    for global_list in g_list:
-        if first_time:
-            sorted_feature_list = dict(sorted(global_list.items(), key=lambda item: item[1], reverse=True))
-            n_important = math.isqrt(len(sorted_feature_list))
-            items = list(sorted_feature_list.items())
-            important_features = dict(items[:n_important])
-            unimportant_features = dict(items[n_important:])
-            first_time = False
-        else:
-            for feat in important_features.keys():
-                if feat in updated_global_feature_list:
-                    important_features[feat] = updated_global_feature_list.pop(feat)
-            unimportant_features = updated_global_feature_list
-
-        # Common steps
-        unimp_feature_size_before = len(unimportant_features)
-        imp_feature_size_before = len(important_features)
-        remove_low_unimportant()
-        promote_features()
-
-        del_u = len(important_features) - imp_feature_size_before
-        del_v = len(unimportant_features) - unimp_feature_size_before
-        print("del-u", del_u)
-        print("del-v", del_v)
-        print("Imp feature", important_features)
-        print("Unimp feature", unimportant_features)
+common_imp_unimp = {}
+updated_global_list = {}
+first_time = True
 
 
+def compute(global_list):
+    global important_features, unimportant_features, del_u, del_v, common_imp_unimp, updated_global_list,first_time
+    if first_time:
+        sorted_feature_list = dict(sorted(global_list.items(), key=lambda item: item[1], reverse=True))
+        n_important = math.isqrt(len(sorted_feature_list))
+        items = list(sorted_feature_list.items())
+        important_features = dict(items[:n_important])
+        unimportant_features = dict(items[n_important:])
+        first_time = False
+    else:
+        for feat in global_list.keys():
+            if feat in important_features:
+                important_features[feat] = global_list[feat]
+            else:
+                unimportant_features[feat] = global_list[feat]
+
+    unimp_feature_size_before = len(unimportant_features)
+    imp_feature_size_before = len(important_features)
+    print("Imp feature", important_features)
+    print("Unimp feature", unimportant_features)
+    remove_low_unimportant()
+    promote_features()
+    del_u = len(important_features) - imp_feature_size_before
+    del_v = len(unimportant_features) - unimp_feature_size_before
+    print("Imp feature", important_features)
+    print("Unimp feature", unimportant_features)
+    print("del-u", del_u)
+    print("del-v", del_v)
 
 def remove_low_unimportant():
     global unimportant_features
